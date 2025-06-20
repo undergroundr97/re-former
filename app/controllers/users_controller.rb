b712078e9861c5c7bd0b9876d63bc63f974876aa
undergroundr97 <123432660+undergroundr97@users.noreply.github.com>
2025-06-20 19:44:39 -0300

    fixed the issue where the users were created over and over if there was an existing username

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,18 +2,42 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
   def create
-    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
-    @user = User.new(user_params)
-    if @user.save
-      redirect_to new_user_path
+    existing_user = User.find_by(username: user_params[:username])
+    # @user = User.new(user_params)
+    if existing_user
+      existing_user.update(user_params)
+      redirect_to new_users_path
     else
-      render :new, status: :unprocessable_entity
+      @user = User.new(user_params)
+      if @user.save
+        redirect_to new_users_path, notice: "wel done"
+      else
+        render new_users_path, notice: "error"
+      end
     end
   end
 
+
+  def edit
+  end
+  def update
+    @user = User.find(params[:username])
+    if @user.update(user_params)
+      redirect_to new_users_path, notice: "User updated successfully."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
   private
+
   def user_params
-    params.expect(user: [ :username, :password, :email ])
+    params.require(:user).permit(:username, :password, :email)
   end
 end
